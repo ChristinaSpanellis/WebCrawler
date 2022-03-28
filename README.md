6c4019ea645b89708bdda889bf9442d42734ecbe
Christina Spanellis <crissispanellis@gmail.com>
2022-03-29 00:18:20 +0200

    Added optimizations and updated README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # WebCrawler
-Basic web crawler to extract nested URLs in a requested web page.
+Basic web crawler to extract and return nested URLs in a requested web page.
+Language: Python 3.8
+
+# Usage
+
+## Using Docker
+
+First build the image 
+```docker build --tag web-crawler .  ```
+
+Start a container and run the image as follows, replacing {url} with your URL of choice.
+```docker run web-crawler {url}```
+
+## Without Docker
+
+To run the code you'll first need to install a packages. [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) is a Python library for extracting data from HTML files.
+
+```pip3 install beautifulsoup4```
+
+Run the code as follows, replaing {url} with your URL of choice.
+```python3 crawler.py {url}```
--- crawler.py
+++ crawler.py
@@ -1,46 +1,60 @@
-import urllib.request, urllib.parse, validators
+from urllib.error import HTTPError, URLError
+import urllib.request, urllib.parse, validators, sys
 from bs4 import BeautifulSoup
 from queue import Queue
 
-crawl_limit = 100
-
-def crawl(URL):
-    urls_to_crawl = Queue(maxsize=100)
+def crawl(URL, crawl_limit = 100):
+    '''
+    Crawls a given URL for further valid URLS, searching recursively through each subpage for more URLs.
+    :param URL: (string) seed URL
+    :param crawl_limit: (int) the number of unique URLs to be crawled, the default is 100
+    :return: (set) crawled urls
+    '''
+    urls_to_crawl = Queue(maxsize=crawl_limit) 
+    # set the queue size to crawl_limit to save time on pages 
+    # with 100s of nested urls since only 100 unique urls need to be returned, this can be increased or ommitted to explore every possible url
     urls_to_crawl.put(URL)
-    urls_crawled = {URL}
+    urls_crawled = {URL} # using a set to store URLs to avoid reporting duplicate URLs
     while (len(urls_crawled) != crawl_limit and not urls_to_crawl.empty()):
         next_url = urls_to_crawl.queue[0]
         try:
             response = urllib.request.urlopen(next_url)
-            html_content = BeautifulSoup(response.read(), 'html.parser')
-            for url in html_content.find_all('a'):
-                if (not urls_to_crawl.full()):
-                    potential_url = url.get('href')
-                    if (potential_url != next_url):
-                        parsed_url = urllib.parse.urlparse(potential_url)
-                        if (parsed_url.netloc):
-                            urls_to_crawl.put(potential_url)
-                        elif (parsed_url.path):
-                            urls_to_crawl.put(next_url + "/" + potential_url)
-                        elif (parsed_url.query):
-                            urls_to_crawl.put(next_url + "/" +potential_url)
+            html_content = BeautifulSoup(response.read().decode('UTF-8', errors='ignore'), 'html.parser')
+            if (not urls_to_crawl.full()):
+                for url in html_content.find_all(href=True):
+                    if (not urls_to_crawl.full()): # check again incase the queue fills up
+                        potential_url = url.get('href')
+                        if (potential_url != next_url):
+                            parsed_url = urllib.parse.urlparse(potential_url)
+                            if (parsed_url.netloc):
+                                urls_to_crawl.put(potential_url)
+                            elif (parsed_url.path):
+                                urls_to_crawl.put(next_url + "/" + potential_url)
+                            elif (parsed_url.query):
+                                urls_to_crawl.put(next_url + "/" +potential_url)
             urls_crawled.add(next_url)
             urls_to_crawl.get()
-        except Exception as e:
+        except HTTPError as e: # I decided to include URLs that return HTTP errors since the spec didn't specify whether only accessible URLs should be reported or not
             urls_crawled.add(next_url)
+            urls_to_crawl.get()
+        except URLError as e: # ignores URLs that are malformed or return non-http errors
+            pass
             urls_to_crawl.get()
         if (len(urls_crawled) % 10 == 0):
             print(str(len(urls_crawled))+"/100 URLs crawled")
     return urls_crawled
 
 def print_urls(urls_crawled):
-    count = 1
-    for url in urls_crawled:
-        print(count + ": ", url)
-        count += 1
+    for count, url in enumerate(urls_crawled):
+        print(str(count + 1) + ": " + url)
 
 def main():
-    URL = str(input())
+    if (not sys.argv[1]):
+        print("Please specify a URL")
+        exit()
+    URL = sys.argv[1]
+    if (URL[-1] == "/"): # removing the / to allow inputs without / at the end to be valid
+        URL = URL[:-1] # eg. can accept http://google.com/ and http://google.com
     if not validators.url(URL):
         print("Requested URL is malformed")
         exit()
